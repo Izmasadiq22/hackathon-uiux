322b81d4de0c036146a15e1d9858adb77dcfc8d2
Izmasadiq22 <izmasadiq44@gmail.com>
2024-12-17 01:45:28 +0500

    Moved components folder to src and updated imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
+import Browse from "@/components/Browse";
+import Funiro from "@/components/Funiro";
+import { Hero } from "@/components/Hero";
+import OurProducts from "@/components/OurProducts";
+import { RoomInspiration } from "@/components/RoomInspiration";
 import React from "react";
-// import { Header } from "./components/Header";
-import { Hero } from "./components/Hero";
-import Browse from "./components/Browse";
-import Funiro from "./components/Funiro";
-import OurProducts from "./components/OurProducts";
-import { RoomInspiration } from "./components/RoomInspiration";
-// import Footer from "./components/Footer";
 
 
 export default function page (){
